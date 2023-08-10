21a8aa7678efd1ef21665769c9aeaf5867045f54
Kristofer Tengstrom <info@kristofer-tengstrom.se>
2023-08-10 17:21:57 +0200

    Add page for list

---
+++ src/Controller/TengstromPartialsController.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\tengstrom_partials\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+class TengstromPartialsController extends ControllerBase {
+
+  public function list(): array {
+    return [];
+  }
+
+}

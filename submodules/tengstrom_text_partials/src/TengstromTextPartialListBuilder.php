557c2381ffb25b2fe44a577180144047193a54d0
Kristofer Tengström <ordermind@gmail.com>
2025-07-23 16:33:16 +0200

    Simplify tengstrom_partials module

--- submodules/tengstrom_text_partials/src/TengstromTextPartialListBuilder.php
+++ submodules/tengstrom_text_partials/src/TengstromTextPartialListBuilder.php
@@ -17,8 +17,7 @@ class TengstromTextPartialListBuilder extends ConfigEntityListBuilder {
    */
   public function buildHeader() {
     $header['label'] = $this->t('Label');
-    $header['id'] = $this->t('Machine name');
-    $header['status'] = $this->t('Status');
+
     return $header + parent::buildHeader();
   }
 
@@ -28,9 +27,17 @@ class TengstromTextPartialListBuilder extends ConfigEntityListBuilder {
   public function buildRow(EntityInterface $entity) {
     /** @var \Drupal\tengstrom_text_partials\TengstromTextPartialInterface $entity */
     $row['label'] = $entity->label();
-    $row['id'] = $entity->id();
-    $row['status'] = $entity->status() ? $this->t('Enabled') : $this->t('Disabled');
+
     return $row + parent::buildRow($entity);
+  }
+
+  public function render(): array {
+    $build = parent::render();
+
+    $build['table']['#type'] = 'accordion_table';
+    $build['table']['#separate_operations'] = TRUE;
+
+    return $build;
   }
 
 }
